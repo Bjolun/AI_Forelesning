4ea04d58fd1b8bbe92d7891d71094018ad730349
Bjørn Arild Lunde <bjolun5@gmail.com>
2025-03-13 14:39:19 +0100

    Commit fra første forelesning

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    // Referanse til agenten vår
+    private NavMeshAgent agent;
+
+    // Referanse til patrol points
+    [SerializeField] private Transform[] patrolPoints;
+
+    // Vite hvilket patrol point vi er på vei til nå
+    private int currentPatrolPoint = 0;
+
+    // Referanse til målet agenten skal følge
+    [SerializeField] private Transform followTarget;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Kontinuerlig gi agenten vår et mål den skal prøve å nå
+    private void Update()
+    {
+ 
+    }
+
+    // Metode for Patrolling
+    private void Patrolling()
+    {
+        // Vite hvor mange patrol points som finnes i arrayen vår.
+        int numberOfPatrolPoints = patrolPoints.Length;
+
+        // Hvis vi nærmer oss neste patrol point, så vil setter vi nytt mål
+        if(agent.remainingDistance < 0.5f)
+        {
+            // Hvis nåværende mål er det siste i arrayen, så vil vi gjøre noe
+            if(currentPatrolPoint + 1 == numberOfPatrolPoints)
+            {
+                // Sette currentPatrolPoints tilbare til 0
+                currentPatrolPoint = 0;
+                // Sett currentPatrolPoint (dom nå er 0) til vårt nye mål
+                agent.SetDestination(patrolPoints[0].position);
+            } else
+            {
+                // Øker vi currentPatrolPoint med 1
+                currentPatrolPoint++;
+                // Sett currentPatrolPoint som det nye målet vårt.
+                agent.SetDestination(patrolPoints[currentPatrolPoint].position);
+            }
+        }
+    }
+
+    // Metode for Chasing
+    private void Chasing()
+    {
+        agent.SetDestination(followTarget.position);
+    }
+
+
+}
